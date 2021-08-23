2a0517eb8f22152f34faf14174ef8721e0fc51f7
Jawad Ahmed Khan <88373522+jawad-unmarshal@users.noreply.github.com>
2021-08-23 18:18:23 +0530

    feat(attendant): attendant parking and unparking of any vehicle

--- parkingLot.go
+++ parkingLot.go
@@ -1,22 +1,114 @@
 package parkingProject
 
+import (
+	"errors"
+	"reflect"
+)
+
+type Subscriber interface {
+	NotifyIsFull()
+	NotifyIsAvailable()
+}
+
 type Vehicle struct {
+	i int
+}
+type Owner struct {
+	IsFull bool
+	Attendant
+}
+
+type Attendant struct {
+	*ParkingLot
+}
+
+func NewAttendant(parkingLot *ParkingLot) *Attendant {
+	return &Attendant{ParkingLot: parkingLot}
+}
+
+func (a Attendant) Park(vehicle *Vehicle) error {
+	lot := a.ParkingLot
+	return lot.Park(vehicle)
+
+}
+
+type PolicePerson struct {
+	IsFull bool
+}
+
+func NewPolicePerson() *PolicePerson {
+	return &PolicePerson{IsFull: false}
+}
+
+func NewOwner() *Owner {
+	return &Owner{IsFull: false}
 }
 
 type ParkingLot struct {
-	ParkingSpace []*Vehicle
+	parkingSpace   []*Vehicle
+	availableSlots int
+	subscribers    []Subscriber
+}
+
+func NewParkingLot(availableSlots int, subscriberList []Subscriber) *ParkingLot {
+	return &ParkingLot{availableSlots: availableSlots, subscribers: subscriberList}
+}
+func (o *Owner) NotifyIsFull() {
+	o.IsFull = true
+}
+
+func (o *Owner) NotifyIsAvailable() {
+	o.IsFull = false
+}
+
+func (p *PolicePerson) NotifyIsFull() {
+	p.IsFull = true
+}
+
+func (p *PolicePerson) NotifyIsAvailable() {
+	p.IsFull = false
 }
 
 func NewVehicle() *Vehicle {
 	return &Vehicle{}
 }
 
-func (P *ParkingLot) Park(vehicle *Vehicle) {
-	P.ParkingSpace = append(P.ParkingSpace, vehicle)
+//func (P ParkingLot) NotifyAllSubsIsFull() {
+//	for _, subscriber := range P.subscribers {
+//		subscriber.notifyIsFull()
+//
+//	}
+//}
+//func (P ParkingLot) NotifyAllSubsIsAvailable() {
+//	for _, subscriber := range P.subscribers {
+//		subscriber.NotifyIsAvailable()
+//
+//	}
+//}
+
+func (P ParkingLot) NotifyAllSubs(functionName string) {
+	for _, subscriber := range P.subscribers {
+		reflect.ValueOf(subscriber).MethodByName(functionName).Call(nil)
+
+	}
+
+}
+
+func (P *ParkingLot) Park(vehicle *Vehicle) error {
+	if P.availableSlots == 0 {
+		return errors.New("Parking is full. Cannot park vehicle")
+	}
+	P.parkingSpace = append(P.parkingSpace, vehicle)
+	P.availableSlots--
+	if P.availableSlots == 0 {
+		P.NotifyAllSubs("NotifyIsFull")
+		//P.NotifyAllSubsIsFull()
+	}
+	return nil
 }
 
 func (P *ParkingLot) IsParked(vehicle *Vehicle) bool {
-	for _, parkedVehicle := range P.ParkingSpace {
+	for _, parkedVehicle := range P.parkingSpace {
 		if parkedVehicle == vehicle {
 			return true
 		}
@@ -24,11 +116,23 @@ func (P *ParkingLot) IsParked(vehicle *Vehicle) bool {
 	return false
 }
 
-func (P *ParkingLot) UnPark(vehicle *Vehicle) {
-	for i, parkedVehicle := range P.ParkingSpace {
-		if parkedVehicle == vehicle {
-			P.ParkingSpace = append(P.ParkingSpace[:i], P.ParkingSpace[1+i:]...)
+func (P *ParkingLot) UnPark(vehicle *Vehicle) error {
+	for i, parkedVehicle := range P.parkingSpace {
+		if parkedVehicle != vehicle {
+			continue
 		}
-	}
 
+		P.unparkVehicleAt(i)
+		P.availableSlots++
+		if P.availableSlots == 1 {
+			P.NotifyAllSubs("NotifyIsAvailable")
+		}
+		return nil
+	}
+	return errors.New("Vehicle has not been parked yet")
+}
+
+func (P *ParkingLot) unparkVehicleAt(i int) {
+	P.parkingSpace[i] = P.parkingSpace[len(P.parkingSpace)-1]
+	P.parkingSpace = P.parkingSpace[:len(P.parkingSpace)-1]
 }
--- parkingLot_test.go
+++ parkingLot_test.go
@@ -26,6 +26,16 @@ func TestPark(t *testing.T) {
 		assert.False(t, isParked)
 	})
 
+	t.Run("Expect error when trying to unpark a vehicle that is not parked", func(t *testing.T) {
+		var vehicle = NewVehicle()
+		var ParkLot ParkingLot
+		result := ParkLot.UnPark(vehicle)
+
+		if result == nil {
+			t.Fatalf("Should not be able to unpark a vehicle that is not parked")
+		}
+	})
+
 	t.Run("Expect first parked vehicle to be unparked", func(t *testing.T) {
 		var vehicle1 = NewVehicle()
 		var vehicle2 = NewVehicle()
@@ -35,9 +45,98 @@ func TestPark(t *testing.T) {
 		ParkLot.Park(vehicle2)
 		ParkLot.Park(vehicle3)
 		ParkLot.UnPark(vehicle2)
-		isParked := ParkLot.IsParked(vehicle1)
+		isParked := ParkLot.IsParked(vehicle2)
 
 		assert.False(t, isParked)
+	})
+
+	t.Run("Expect parkinglot with capacity of 2 to be full after 2 vehicles are parked", func(t *testing.T) {
+		owner := NewOwner()
+		subList := make([]Subscriber, 1)
+		subList[0] = owner
+		parkingLot := NewParkingLot(2, subList)
+		for i := 0; i < 2; i++ {
+			vehicle := NewVehicle()
+			parkingLot.Park(vehicle)
+		}
+
+		result := owner.IsFull
+
+		assert.True(t, result, "Parking lot should be full")
+	})
+
+	t.Run("Expect to notify owner when slots become available", func(t *testing.T) {
+		var subList = make([]Subscriber, 1)
+		owner := NewOwner()
+		subList[0] = owner
+		parkingLot := NewParkingLot(2, subList)
+
+		vehicle1 := NewVehicle()
+		parkingLot.Park(vehicle1)
+		vehicle2 := NewVehicle()
+		parkingLot.Park(vehicle2)
+
+		parkingLot.UnPark(vehicle1)
+
+		result := owner.IsFull
+
+		assert.Equal(t, false, result, "Parking lot should have space")
+	})
+
+	t.Run("Notify Policeman that slot is full", func(t *testing.T) {
+		policeMen := NewPolicePerson()
+		var subList = make([]Subscriber, 1)
+		subList[0] = policeMen
+		parkingLot := NewParkingLot(2, subList)
+
+		vehicle1 := NewVehicle()
+		parkingLot.Park(vehicle1)
+		vehicle2 := NewVehicle()
+		parkingLot.Park(vehicle2)
+
+		result := policeMen.IsFull
+		assert.Equal(t, true, result, "Parking lot should be full")
+
+	})
+
+	t.Run("Expect an attendant to park vehicle", func(t *testing.T) {
+		var subList = make([]Subscriber, 1)
+		owner := NewOwner()
+		subList[0] = owner
+		parkingLot := NewParkingLot(2, subList)
+		attendant := NewAttendant(parkingLot)
+		vehicle1 := NewVehicle()
+
+		parkResult := attendant.Park(vehicle1)
+
+		if parkResult != nil {
+			t.Fatalf("attendant failed to park: ", parkResult)
+		}
+
+		isParked := parkingLot.IsParked(vehicle1)
+
+		assert.Equal(t, true, isParked, "Vehicle should be parked")
+
+	})
+
+	t.Run("Expect an attendant to Unpark vehicle", func(t *testing.T) {
+		var subList = make([]Subscriber, 1)
+		owner := NewOwner()
+		subList[0] = owner
+		parkingLot := NewParkingLot(2, subList)
+		attendant := NewAttendant(parkingLot)
+		vehicle1 := NewVehicle()
+		attendant.Park(vehicle1)
+		parkResult := attendant.UnPark(vehicle1)
+
+		if parkResult != nil {
+			t.Fatalf("attendant failed to Unpark: ", parkResult)
+		}
+
+		isParked := parkingLot.IsParked(vehicle1)
+
+		assert.Equal(t, true, isParked, "Vehicle should be Unparked")
+
 	})
 
 }
